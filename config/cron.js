1846fcb998fc24625a1da0b7e9e38a12f8ad18f9
sudo-dpkg <adarshkumarmsay@gmail.com>
2025-03-21 11:02:43 +0530

    cron.js updated

--- config/cron.js
+++ config/cron.js
@@ -1,11 +1,19 @@
-import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
-    https
+if (!process.env.API_URL) {
+  console.error("API_URL is not defined");
+  process.exit(1);
+}
+
+const job = new CronJob("*/14 * * * *", function () {
+  https
     .get(process.env.API_URL, (res) => {
-        if(res.statusCode === 200) console.log("GET request sent successfully");
-        else console.log("GET request failed", res.statusCode);
+      if (res.statusCode === 200) {
+        console.log("GET request sent successfully");
+      } else {
+        console.log("GET request failed", res.statusCode);
+      }
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
